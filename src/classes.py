538d1e7bed1b403cc2a8f1f4dd766b4907a7ae44
Alina Davydenko <Alinadavydenko620@gmail.com>
2024-11-18 22:51:46 +0300

    istaks

--- src/classes.py
+++ src/classes.py
@@ -11,20 +11,26 @@ class Product:  # Create an empty class
         self.__price = price
         self.quantity = quantity
 
+    @property
+    def price(self):
+        return self.__price
+
     @classmethod
-    def new_product(cls, dict_of_products):
-        print(Category.products)
-        for element in Category.__products:
-            if dict_of_products['name'] == element['name']:
+    def new_product(cls, dict_of_products: dict):
+        products_list = [Category.products]
+        for product in products_list:
+            if dict_of_products['name'] == product:
                 quantity = dict_of_products['quantity'] + Category.products['quantity']
                 cls.quantity = quantity
             else:
                 cls.quantity = dict_of_products['quantity']
-        name = dict_of_products['name']
-        description = dict_of_products['description']
-        price = dict_of_products['price']
-        quantity = dict_of_products['quantity']
-        return cls(name, description, price, quantity)
+        for product in products_list:
+            max_price = max(dict_of_products['price'], product['price'])
+            cls.__price = max_price
+        cls.name = dict_of_products['name']
+        cls.description = dict_of_products['description']
+        # cls.__price = dict_of_products['price']
+        cls.quantity = dict_of_products['quantity']
 
     @property
     def price(self):
@@ -52,23 +58,23 @@ class Category:
     """ class for categories """
     name: str
     description: str
-    __products: list
+    __products = list()
 
-    __products = []
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
         self.__products = products
+        Category.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
 
     @property
-    def products(self):
-        return self.__products
+    def product(self):
+        return Category.__products
 
     @staticmethod
     def add_product(new_product: Product):
@@ -76,46 +82,9 @@ class Category:
         Category.product_count += 1
 
     @property
-    def product_list(self):
+    def products(self) -> list[str]:
         """ геттер, который возвращает список продуктов """
-        list_of_product = ''
-        for product in self.__products:
-            list_of_product += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
-        return list_of_product
-
-
-# проверка
-if __name__ == "__main__":
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
-
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(Category.add_product(new_product))
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+        list_of_products = list()
+        for product in Category.__products:
+            list_of_products.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n')
+        return list_of_products
