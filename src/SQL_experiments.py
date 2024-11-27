e34ffc0fa20beb223631bbd6bb9df540677e483b
Alina Davydenko <Alinadavydenko620@gmail.com>
2024-11-27 21:36:15 +0300

    add main.py

--- src/SQL_experiments.py
+++ src/SQL_experiments.py
@@ -1,23 +1,23 @@
 import psycopg2
 
 # подключение к базе данных
-connection = psycopg2.connect(
-    host='localhost',
-    database='postgres',
-    user='postgres',
-    password='81726354'
-)
+# connection = psycopg2.connect(
+#     host='localhost',
+#     database='postgres',
+#     user='postgres',
+#     password='81726354'
+# )
 
 # операции с базой данных
-cur = connection.cursor()
-cur.execute('INSERT INTO post VALUES (%s, %s, %s)', (8, 'G', 'l'))
-cur.execute('SELECT * FROM post')
-connection.commit()
-
-
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
-cur.close()
-connection.close()
+# cur = connection.cursor()
+# cur.execute('INSERT INTO post VALUES (%s, %s, %s)', (8, 'G', 'l'))
+# cur.execute('SELECT * FROM post')
+# connection.commit()
+#
+#
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
+#
+# cur.close()
+# connection.close()
